505dfd2a578a06007470e9b9110dc0ffc405e419
Jiun <jiun48562@gmail.com>
2024-01-14 22:46:41 +0900

    [ADD] Shopping App and User Model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "150자 미만의 영문, 숫자,  @/./+/-/_ 기호만 포함해야 합니다."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("이미 존재하는 아이디입니다."),
+        },
+    )
+    email = models.EmailField(_("email address"), blank=True)
+    name = models.CharField(_("name"), max_length=150, blank=True)
+    address = models.CharField(_("address"), max_length=1024, blank=True)
+    zip_code = models.CharField(_("zip code"), max_length=10, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    point = models.IntegerField(default=0)
+    is_seller = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "name", "address", "zip_code"]
+
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #local apps
     'accounts',
+    'shopping',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
